39219028377e3cd0db617d15a33e3e776a58e79c
gabrirusso <91951339+gabrirusso@users.noreply.github.com>
2021-12-11 18:33:52 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+#include <cstring>
 
+bool funzione (char listanomi[10][20],char nome[1][20], int &k ){
+    for (int i=0; i<10; i++){
+        if (strcmp(listanomi[i], nome[0])==0){
+            k=i+1;
+            return true;
+        }
+    }
+    return false;
+}
