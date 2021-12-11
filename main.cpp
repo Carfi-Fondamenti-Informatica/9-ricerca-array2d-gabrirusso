7feded0ca7f6f147ce0cb75dc8d8c8a8f1f59dca
gabrirusso <91951339+gabrirusso@users.noreply.github.com>
2021-12-11 18:32:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
-}
+using namespace std;
+
+
+    int main() {
+        int k = 0;
+
+
+        char nome[1][20];
+
+        char listanomi[10][20];
+        for (int i = 0; i < 10; i++) {
+
+            cin >> listanomi[i];
+
+        }
+
+        cin >> nome[0];
+
+        if (funzione(listanomi,nome, k )==true){
+            cout << k ;
+        } else {
+            cout << "non presente";
+        }
+
+        return 0;
+    }
+
